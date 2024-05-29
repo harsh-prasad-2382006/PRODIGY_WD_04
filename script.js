34dce85c40da95a41027b848de6b5d029e9b650e
Harsh Prasad <harshprasad2382006@gmail.com>
2024-05-29 08:58:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings : ["Front-end Developer", "Web-designer", "Programmer"],
+    typeSpeed : 80,
+    backSpeed : 80,
+    backdelay : 800,
+    loop : true
+});     
+                    
